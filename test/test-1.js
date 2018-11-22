a78fa92220aebc7ad2c7232337fed05b83bb4f74
apporoad <apporoadina@163.com>
2018-11-22 17:15:55 +0800

    editsome

--- test/test-1.js
+++ test/test-1.js
@@ -56,3 +56,28 @@ engine.run(lustAddr,{ config: {verbose:false}}).then(data => {
     console.log(data)
 })
 
+
+//USD
+var lustUSD ={
+    isLust: true,
+    input:{
+        type: "file",
+        path:  __dirname + "/data/test-1.txt",
+        encode: "utf8"
+    },
+    finder: {
+        type:"text",
+        position:{
+            after:["/SHIP\\s{1,3}TO/"],
+            before:["SHIPPED","/ON\\W{1,3}ABOUT/"]
+        }
+    },
+    extractor:{
+        type:"text"
+        //regEx:"H\\d{8}"
+    }
+}
+
+engine.run(lustUSD,{ config: {verbose:false}}).then(data => {
+    console.log(data)
+})
