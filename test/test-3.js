2e6f881775ff9755b65e16df68ec4222c6c5f5b6
apporoad <apporoadina@163.com>
2018-12-14 11:25:01 +0800

    addsome

--- test/test-3.js
+++ test/test-3.js
@@ -1,7 +1,40 @@
 var xget = require('../index')
 
-xget.get({
-    
-}).then(data=>{
 
+// get input
+xget.get({
+    input : {
+        isLust:true,
+        input:{
+            type: "file",
+            path:  [__dirname + "/data2/c-0.txt",__dirname + "/data2/c-1.txt",__dirname + "/data2/c-2.txt",__dirname + "/data2/c-3.txt",__dirname + "/data2/c-4.txt"],
+            encode: "utf8",
+            multi:false
+        },
+        filter:[
+            {
+                role: "extractor",
+                type: "ocrPre"
+            }
+        ]
+    }
+}).then(raw=>{
+    var options = { verbose : false}
+    options.input = raw
+
+    //get cooked
+    xget.get({
+        fapiaohao:{
+            isLust:true
+        },
+        gongfangmingcheng:{},
+        shouhuorenxinxi:{},
+        yunshufangshi:{},
+        kongyunri:{},
+        maoyifangshi:{},
+        dingdanhao:{},
+        detals:{}
+    },options).then(cooked =>{
+
+    })
 })
